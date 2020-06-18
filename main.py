67d3afce1a3b35d42a63123b6eb5725f8ddede6a
Teodor Hristov <thristov@abrites.com>
2020-06-18 23:32:26 +0300

    Enemy ships spawning and moving. Lost screen

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ pygame.display.set_caption("Space Ships Shooter Game")
 
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-BLUE = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
-GREEN = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 
 # Player ship
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
@@ -25,6 +25,26 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 # Background
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+# 1:16:30 Lasers
+
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window): 
+        window.blit(self.img, (self.x, self.y))
+    
+    def move(self, vel):
+        self.y += vel
+    
+    def off_screen(self, height):
+        return self.y <= height and self.y >= 0
+    
+    def collision(self, obj):
+        return collide(obj, self)
 
 class Ship:
     def __init__(self, x, y, health=100):
@@ -45,6 +65,22 @@ class Ship:
     def get_height(self):
         return self.ship_img.get_height()
 
+class Enemy(Ship):
+    COLOR_MAP = {
+        "red":(RED_SPACE_SHIP, RED_LASER),
+        "green":(GREEN_SPACE_SHIP, GREEN_LASER),
+        "blue":(BLUE_SPACE_SHIP, BLUE_SPACE_SHIP)
+    }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
+
+
 class Player(Ship):
     def __init__(self, x, y, health=100):
         super().__init__(x, y, health)
@@ -59,10 +95,19 @@ def main():
     lives = 5
     level = 1
     main_font = pygame.font.SysFont("comicsans", 50)
+    lost_font = pygame.font.SysFont("comicsans", 70)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+
     player_vel = 5
     player = Player(300, 650)
-
+    
     clock = pygame.time.Clock()
+
+    lost = False
+    lost_count = 0
 
     def redraw_window():
         WIN.blit(BACKGROUND, (0,0))
@@ -74,13 +119,40 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
+
+        if lost:
+            lost_label = lost_font.render("You LOST!", 1, (255,255,255))
+            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
 
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
         redraw_window()
+
+        if lives <= 0 or player.health <= 0:
+            lost = True
+            lost_count += 1
+
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else:
+                continue
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH-100), 
+                              random.randrange(-1500, -100),
+                              random.choice(["red", "green", "blue"])
+                              )
+                enemies.append(enemy)
 
         for event in pygame.event.get():
             # extension error
@@ -97,5 +169,11 @@ def main():
             player.y -= player_vel
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: # down
             player.y += player_vel
+        
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
 
 main()
